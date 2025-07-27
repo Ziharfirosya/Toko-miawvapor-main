133c9b7478fb69f75709a0d192ce65ecf468a772
Ziharfirosya <147595511+Ziharfirosya@users.noreply.github.com>
2025-07-27 21:13:24 +0700

    Add files via upload

---
+++ application/views/layouts/_footer.php
@@ -0,0 +1,10 @@
+<footer class="footer mt-5">
+	<nav class="navbar navbar-dark bg-light sticky-bottom">
+		<div class="container d-flex justify-content-center">
+			<div class="copyright text-info p-3">
+				<small class="text-info p-3">Copyright &copy; MIAW VAPOR 2025</a>
+				</small>
+			</div>
+		</div>
+	</nav>	
+</footer>  
